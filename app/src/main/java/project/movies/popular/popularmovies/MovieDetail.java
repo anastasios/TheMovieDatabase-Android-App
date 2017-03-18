3c1add85b73c4bbaa52903bef0666484cbda4c99
anastashs <anastashs@gmail.com>
2017-03-18 15:57:22 +0000

    added tab layout

--- app/src/main/java/project/movies/popular/popularmovies/MovieDetail.java
+++ app/src/main/java/project/movies/popular/popularmovies/MovieDetail.java
@@ -1,6 +1,12 @@
 package project.movies.popular.popularmovies;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 
@@ -10,5 +16,41 @@ public class MovieDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new CollectionPagerAdapter(getSupportFragmentManager(), MovieDetail.this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public static class CollectionPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 2;
+        private Context context;
+
+        public CollectionPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            if (i == 0)
+                return new MovieDetailFragment();
+            else
+                return new MovieDetailFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0)
+                return "Movie Details";
+            else
+                return "Movie Reviews";
+        }
     }
 }
